59c548dbd825f702d184dad80a8c179292fb4cf7
Vikas <=>
2015-03-18 17:43:38 +0530

    getRentalFrequentPoints moved to rental class

--- src/com/twu/refactor/Customer.java
+++ src/com/twu/refactor/Customer.java
@@ -10,8 +10,8 @@ public class Customer {
 		this.name = name;
 	}
 
-	public void addRental(Rental arg) {
-		rentalList.add(arg);
+	public void addRental(Rental rental) {
+		rentalList.add(rental);
 	}
 
 	public String getName() {
@@ -24,7 +24,7 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : rentalList){
             double rentalAmount = rental.getRentalAmount();
-            frequentRenterPoints += getFrequentRentalPoints(rental);
+            frequentRenterPoints += rental.getFrequentRentalPoints();
             result += rental.addFigures(rentalAmount);
             totalAmount += rentalAmount;
 		}
@@ -32,22 +32,9 @@ public class Customer {
 		return result;
 	}
 
-    private int getFrequentRentalPoints(Rental rental) {
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-        if (isNewRelease(rental) && rental.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
-    }
-
-    private boolean isNewRelease(Rental rental) {
-        return (rental.getMovie().getPriceCode() == MoviePricingCategory.NEW_RELEASE);
-    }
-
     private String getSummary(double totalAmount, int frequentRenterPoints) {
-        return "Amount owed is " + String.valueOf(totalAmount) + "\n"
-                +"You earned " + String.valueOf(frequentRenterPoints)
-                +" frequent renter points";
+        return "Amount owed is " + totalAmount +"\n" +"You earned "
+                + frequentRenterPoints +" frequent renter points";
     }
 
     public String htmlStatement() {
@@ -55,7 +42,7 @@ public class Customer {
         int extraRentalPoints = 0;
         String htmlStatement = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>";
         for (Rental rental : rentalList){
-            extraRentalPoints += getFrequentRentalPoints(rental);
+            extraRentalPoints += rental.getFrequentRentalPoints();
             totalAmount += rental.getRentalAmount();
             htmlStatement += rental.getMovie().getTitle()+": "+
                             rental.getRentalAmount()+ "<BR>";
--- src/com/twu/refactor/Rental.java
+++ src/com/twu/refactor/Rental.java
@@ -24,4 +24,15 @@ public class Rental {
         return "\t" + getMovie().getTitle() + "\t"
                 + String.valueOf(thisAmount) + "\n";
     }
+
+    public boolean isNewRelease() {
+        return (movie.getPriceCode() == MoviePricingCategory.NEW_RELEASE);
+    }
+
+    public int getFrequentRentalPoints() {
+        int frequentRenterPoints = 1;
+        if (isNewRelease() && getDaysRented()> 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
